3cea7c7cb20d3b49fae541228900d3e8497e581c
Justin Elliott <Justinelliott11375@gmail.com>
2018-10-21 14:43:11 -0500

    1st production deployment

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const appConfig = require("./config/main-config.js");
+const routeConfig = require("./config/route-config.js");
+const secretKey = process.env.secretTestKey;
+
+appConfig.init(app, express);
+routeConfig.init(app);
+
+
+module.exports = app;
