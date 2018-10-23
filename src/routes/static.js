9d4c0eba60771a1895100905b0c161877b28c681
Justin Elliott <Justinelliott11375@gmail.com>
2018-10-22 19:10:48 -0500

    workable index page layout, partials

--- src/routes/static.js
+++ src/routes/static.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-//const staticController = require("../controllers/staticController");
+const staticController = require("../controllers/staticController");
 
-//router.get("/", staticController.index);
-router.get("/", (req, res, next) => {
-    res.send("Welcome!");
-  });
+router.get("/", staticController.index);
+//router.get("/", (req, res, next) => {
+  //  res.send("Welcome!");
+  //});
 module.exports = router;
